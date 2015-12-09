b128feeb808d1aebaf06e2ce90be2210c5d33fdf
Martang <martijndelange1@gmail.com>
2015-12-09 22:24:19 +0100

    de kamer format+data

---
+++ Martijn/my_data.py
@@ -0,0 +1,19 @@
+my_room = {
+    
+		'plaats': "Arnhem", 
+		'straat naam': "Kastanjelaan 17" , 
+		'postcode': "6828 GH",
+		'huisnummer':17,
+		'opp':21 ,
+		'hoogte':3.10 , 
+		'lengte':3.50 , 
+		'breedte':6,
+		'banken':0 ,
+	    'bureaus': 1 ,
+		'kasten': 1,
+		'deur': 2 ,
+		'ramen': 2,
+	    'bed': 1,
+		
+	}
+
