04bc7c9bfe017f981a73a55b51587343725a2159
FlorisVersteeg <fsversteeg@gmail.com>
2015-12-10 13:56:24 +0100

    edit 2

---
+++ Floris/dexter.py
@@ -0,0 +1,94 @@
+serie = {
+	'seasons': [
+		{
+			'name': 'S01', 
+			'year': 2006, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': "Pilot"},
+				{'name': 's01e01', 'title': "Crocodile"},
+				{'name': 's01e01', 'title': "Popping Cherry"},
+				{'name': 's01e01', 'title': "Let's give the boy a hand"}
+			]
+		}, 
+		{
+			'name': 'S02', 
+			'year': 2007, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""}
+			]
+		}, 
+		{
+			'name': 'S03', 
+			'year': 2008, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""}
+			]
+		},  
+		{
+			'name': 'S04', 
+			'year': 2009, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""}
+			]
+		}, 
+		{
+			'name': 'S05', 
+			'year': 2010, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""}
+			]
+		}, 
+		{
+			'name': 'S06', 
+			'year': 2011, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""}
+			]
+		}, 
+		{
+			'name': 'S07', 
+			'year': 2012, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""}
+			]
+		},  
+		{
+			'name': 'S08', 
+			'year': 2013, 
+			'director': "James Manos Jr.",
+			'episodes': [
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""},
+				{'name': 's01e01', 'title': ""}
+			]
+		} 
+	], 
+	'main character': 'Dexter Morgan', 
+	'title': 'Dexter'
+}
