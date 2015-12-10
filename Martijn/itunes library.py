ebdd77da8521327a3e0e6c7ea00cff27af85f461
Martang <martijndelange1@gmail.com>
2015-12-10 09:29:49 +0100

    library

---
+++ Martijn/itunes library.py
@@ -0,0 +1,135 @@
+Muziek = {
+    'albums': [
+        {
+            'name':'Caracal (Deluxe)', 
+            'tracks': [
+                {
+                    'track':"1-11",
+                    'titel':'nocturnal',
+                    'airdate': "2010",
+                    'duur':"3;50"
+                },
+                
+                {
+                    'track':"1-11",
+                    'titel':'omen',
+                    'airdate': "2010",
+                    'duur':"3;54"
+                },
+                
+               {
+                    'track':"1-13",
+                    'titel':'holding on',
+                    'airdate': "2010",
+                    'duur':"5;15"
+                },
+                
+                {
+                    'track':"1-14",
+                    'titel':'willing & able',
+                    'airdate': "2010",
+                    'duur':"4;15"
+                },
+                
+                {
+                    'track':"1-15",
+                    'titel':'magnets',
+                    'airdate': "2010",
+                    'duur':"4;25"
+                },
+                
+                {
+                    'track':"1-16",
+                    'titel':'jaded',
+                    'airdate': "2010",
+                    'duur':"2;25"
+                },
+                
+                {
+                    'track':"1-17",
+                    'titel':'good itentions',
+                    'airdate': "2010",
+                    'duur':"2;40"
+                },
+                
+                {
+                    'track':"1-19",
+                    'titel':'superego',
+                    'airdate': "2010",
+                    'duur':"2;10"
+                },
+                
+                {
+                    'track':"1-20",
+                    'titel':'hourglass',
+                    'airdate': "2010",
+                    'duur':"2;10"
+                },
+                
+                {
+                    'track':"1-21",
+                    'titel':'echoes',
+                    'airdate': "2010",
+                    'duur':"6;10"
+                },
+                
+                {
+                    'track':"1-22",
+                    'titel':'masterpiece',
+                    'airdate': "2010",
+                    'duur':"4;10"
+                },
+                
+                {
+                    'track':"1-23",
+                    'titel':'masterpiece',
+                    'airdate': "2010",
+                    'duur':"4;10"
+                },
+                
+                 {
+                    'track':"1-24",
+                    'titel':'molecules',
+                    'airdate': "2011",
+                    'duur':"1;10"
+                },
+                
+                {
+                    'track':"1-25",
+                    'titel':'moving mountains',
+                    'airdate': "2011",
+                    'duur':"1;10"
+                },
+                
+                {
+                    'track':"1-26",
+                    'titel':'bang that',
+                    'airdate': "2011",
+                    'duur':"3;10"
+                },
+                
+                {
+                    'track':"1-27",
+                    'titel':'bang that',
+                    'airdate': "2011",
+                    'duur':"3;10"
+                },
+                
+                {
+                    'track':"1-28",
+                    'titel':'bang that',
+                    'airdate': "2011",
+                    'duur':"4;25"
+                },
+                
+                {
+                    'track':"1-29",
+                    'titel':'afterthought',
+                    'airdate': "2011",
+                    'duur':"4;25"
+                },
+            ]
+            
+        },
+    ]
+}
