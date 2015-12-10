2333f5442ee82a36e34bd0296b460be4b5522943
FlorisVersteeg <fsversteeg@gmail.com>
2015-12-10 11:29:40 +0100

    edit

---
+++ Floris/itunes.py
@@ -0,0 +1,93 @@
+
+
+Album = {
+
+'artist': 'Justin Bieber',
+'album': 'Purpose (Deluxe)',
+'date' : '2015',
+'total': '10 songs',
+'lenght': '1 hr 9min',
+'tracks':[
+    {
+    'name': 'Mark My Words',
+    'duration': '2:14',
+    'likes': '3bar',
+    
+    },
+    
+    {
+
+    'name': 'Ill Show You',
+    'duration': '3:20',
+    'likes': '4bar',
+    
+    },
+
+    {
+
+    'name': 'What Do You Mean?',
+    'duration': '3:26',
+    'likes': '5bar',  
+
+    },
+
+    {
+
+    'name': 'Sorry',
+    'duration': '3:21',
+    'likes': '5bar',
+
+
+    },
+    {
+
+    'name': 'Love Yourself',
+    'duration': '3:54',
+    'likes': '5bar',
+
+
+    },
+    {
+
+    'name': 'Company',
+    'duration': '3:28',
+    'likes': '2bar',
+
+    },
+    
+     {
+
+    'name': 'No Pressure',
+    'duration': '4:47',
+    'likes': '3bar',
+
+    },
+    
+      {
+
+    'name': 'No Sense',
+    'duration': '4:35',
+    'likes': '2bar',
+
+    },
+    
+      {
+
+    'name': 'The Feeling',
+    'duration': '4:05',
+    'likes': '5bar',
+
+    },
+    
+    {
+
+    'name': 'Life Is Worth Living',
+    'duration': '3:55',
+    'likes': '5bar',
+
+    },
+    
+    ]
+    
+    }
+    
---
+++ Floris/myroom.py
@@ -0,0 +1,26 @@
+d = {'name': "Floris Simon Versteeg",
+     'adress': "kerkplantsoen",
+     'city': "Spankeren",
+     'zipcode': "6956",
+     'room mates': "my mother",
+     'square meters': "21 square meters",
+     'windows': "2 windows",
+     'wall color': "lightgreen",
+  
+
+     
+     
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
