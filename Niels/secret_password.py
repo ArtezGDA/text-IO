e231384c970a9432ccc38545131f7bcb2e0c6cf7
Niels Otterman <niels.otterman@home.nl>
2015-12-10 15:46:45 +0100

    secret_password

---
+++ Niels/secret_password.py
@@ -0,0 +1,2 @@
+github_account = {'user': "clotterman", 'password': "53807af264cfcb232ababe1f7f9e0e46b7784ca9"}
+g = Github(github_account['clotterman'], github_account['53807af264cfcb232ababe1f7f9e0e46b7784ca9'])
