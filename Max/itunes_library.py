67bb5ac3af7baa92578ed33b80ff57fb14684f81
Max Waelbers <maxwaelbers@MacBook-Pro-van-Max.local>
2015-12-10 11:11:07 +0100

    added files

---
+++ Max/itunes_library.py
@@ -0,0 +1,144 @@
+Album = {
+
+'artist': 'MF DOOM',
+'album': 'Mm.. Food',
+'date' : '2004',
+'total': '15 songs',
+'tracks':
+    [{
+
+    'name': 'Beef Rapp',
+    'duration': '4:39',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Hoe Cakes',
+    'duration': '3:54',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Potholderz (feat. Count Bass D',
+    'duration': '3:20',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'One Beer',
+    'duration': '4:18',
+    'prize': '1,29',
+
+
+    },
+    {
+
+    'name': 'Deep Fried Frenz',
+    'duration': '4:59',
+    'prize': '1,29',
+
+
+            },
+    {
+
+    'name': 'Poo-Putt Platter',
+    'duration': '1:13',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Fillet-O-Rapper',
+    'duration': '1:03',
+    'prize': '1.29',
+
+    },
+
+    {
+
+    'name': 'Gumbo',
+    'duration': '0:49',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Fig Leaf Bi-Carbonate',
+    'duration': '3:19',
+    'prize': '1,29',
+    },
+ 
+    {
+     
+    'name': 'Kon Karne',
+    'duration': '2:51',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Guinessses (feat. 4lze & Angelika)',
+    'duration': '4:41',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Kon Queso',
+    'duration': '4:00',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Rapp Snitch Knishes',
+    'duration': '2:52',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Vomitspit',
+    'duration': '2:48',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Kookies',
+    'duration': '4:01',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Hoe Cakes (Jake One Remix)',
+    'duration': '2:56',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Beef Rap (Live)',
+    'duration': '2:51',
+    'prize': '1,29',
+
+    },
+   ] 
+}
+print Album
---
+++ Max/my_room_data.py
@@ -0,0 +1,19 @@
+
+my_room = {
+    
+		'plaats': "Arnhem", 
+		'straat naam': "beekstraat 24A" , 
+		'postcode': "6811 DV",
+		'huisnummer':24,
+		'opp':20 ,
+		'hoogte':3 , 
+		'lengte':5 , 
+		'breedte':4,
+		'banken':0 ,
+	    'bureaus': 1 ,
+		'kasten': 1,
+		'deur': 1 ,
+		'ramen': 1,
+	    'bed': 1,
+		
+	}
