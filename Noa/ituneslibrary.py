1ab74cf7138edb63c930f632134bdd5bdb787070
Noa Moll <noamoll@hotmail.nl>
2015-12-15 11:49:23 +0100

    homework

---
+++ Noa/ituneslibrary.py
@@ -0,0 +1,105 @@
+Album = {
+
+'artist': 'Glass Animals',
+'album': 'Zaba',
+'date' : '2014',
+'total': '11 songs',
+'tracks':
+    [{
+
+    'name': 'Flipp',
+    'duration': '3:43',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Black Mambo',
+    'duration': '4:09',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Pools',
+    'duration': '4:49',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Gooey',
+    'duration': '4:49',
+    'prize': '1,29',
+
+
+    },
+    {
+
+    'name': 'Walla Walla',
+    'duration': '3:37',
+    'prize': '1,29',
+
+
+            },
+    {
+
+    'name': 'Intruxx',
+    'duration': '2:49',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Hazey',
+    'duration': '4:26',
+    'prize': '1.29',
+
+    },
+
+    {
+
+    'name': 'Toes',
+    'duration': '4:15',
+    'prize': '1,29',
+
+    },
+
+    {
+
+    'name': 'Wyard',
+    'duration': '4:06',
+    'prize': '1,29',
+    },
+ 
+    {
+     
+    'name': 'Kon Karne',
+    'duration': '2:51',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Cocoa Hooves',
+    'duration': '4:32',
+    'prize': '1,29',
+
+    },
+
+    {
+     
+    'name': 'Jdnt',
+    'duration': '4:24',
+    'prize': '1,29',
+
+    },
+
+   ] 
+}
+print Album
---
+++ Noa/my_room_data.py
@@ -0,0 +1,19 @@
+
+my_room = {
+    
+		'plaats': "Arnhem", 
+		'straat naam': "Hommelstraat" , 
+		'postcode': "6828 AJ",
+		'huisnummer':51,
+		'opp':18,
+		'hoogte':3 , 
+		'lengte':4 , 
+		'breedte':4,
+		'banken':0 ,
+	    'bureaus': 1 ,
+		'kasten': 1,
+		'deur': 1 ,
+		'ramen': 1,
+	    'bed': 1,
+		
+	}
