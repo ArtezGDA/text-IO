dc4d5ce252be31e4e2a30a64e3ba3965eab7422f
Lydienne <lydiennealbertoe@MacBook-Pro-van-Lydienne.local>
2015-12-16 17:27:08 +0100

    slaapkamer

---
+++ Lydienne/slaapkamer/slaapkamer.py
@@ -0,0 +1,12 @@
+mijn_slaapkamer = { 
+    'adres': "De Houtmanstraat 129",  
+    'postcode': 6826,  
+    'stad': "Arnhem",  
+    'oppervlakte': 20,
+    'plafon hoogte':2.5, 
+    'deuren': 1, 
+    'ramen':3, 
+    'bedden' : 1, 
+    'koelkast' : 1,  
+    
+}
