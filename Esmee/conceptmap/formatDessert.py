1c36605d25d6b033513370879b19734b138d950a
Esmee Ellson <esmee@ellson.nl>
2016-01-02 00:55:56 +0100

    des

---
+++ Esmee/conceptmap/formatDessert.py
@@ -0,0 +1,75 @@
+import Dessert
+
+d = Dessert.Dessertdict
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Dessert.Dessertdict1
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Dessert.Dessertdict2
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Dessert.Dessertdict3
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Dessert.Dessertdict4
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Dessert.Dessertdict5
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Dessert.Dessertdict6
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Dessert.Dessertdict7
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+print "Desserts zijn 7x in de aanbieding deze week."
