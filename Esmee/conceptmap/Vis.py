d1a16c2744953b2b23e545da957a170f72b3187b
Esmee Ellson <esmee@ellson.nl>
2016-01-02 00:20:41 +0100

    vis

--- Esmee/conceptmap/Vis.py
+++ Esmee/conceptmap/Vis.py
@@ -1,9 +1,7 @@
-Visdict = 
-
-{'supermarkt': "Spar", 'wat': "Princes tonijnstukken", 'korting': "49 cent" , 'hoeveel': "185 gram", 'normaal': "1 euro en 68 cent", 'deze week': "1 euro en 19 cent"}
-{'supermarkt': "Jan Linders", 'wat': "Kibbeling", 'korting': "1 euro en 76 cent" , 'hoeveel': "500 gram", 'normaal': "6 euro en 75 cent", 'deze week': "4 euro en 99 cent"}
-{'supermarkt': "Albert Heijn", 'wat': "haring", 'korting': "25 procent" , 'hoeveel': "2 a 3 stuks", 'normaal': "2 euro en 50 cent", 'deze week': "1 euro en 87 cent"}
-{'supermarkt': "Albert Heijn", 'wat': "Gebakken Kibbeling", 'korting': "25 procent" , 'hoeveel': "150 gram", 'normaal': "3 euro en 25 cent ", 'deze week': "2 euro en 43 cent"}
-{'supermarkt': "Plus", 'wat': "Princes tonijnstukken", 'korting': "45 cent" , 'hoeveel': "160 gram", 'normaal': "1 euro en 45 cent", 'deze week': "1 euro"}
-{'supermarkt': "Plus", 'wat': "Gerookte noorse zalm", 'korting': "2 euro en 1 cent" , 'hoeveel': "pakje 100 gram", 'normaal': "6 euro", 'deze week': "3 euro en 99 cent"}
-{'supermarkt': "Plus", 'wat': "Gijs gerookte makreel filet", 'korting': "50 cent" , 'hoeveel': "pakje 175 gram", 'normaal': "3 euro en 99 cent", 'deze week': "3 euro en 49 cent"}
+Visdict = {'supermarkt': "Spar", 'wat': "Princes tonijnstukken", 'korting': "49 cent" , 'hoeveel': "185 gram", 'normaal': "1 euro en 68 cent", 'deze week': "1 euro en 19 cent"}
+Visdict1 = {'supermarkt': "Jan Linders", 'wat': "Kibbeling", 'korting': "1 euro en 76 cent" , 'hoeveel': "500 gram", 'normaal': "6 euro en 75 cent", 'deze week': "4 euro en 99 cent"}
+Visdict2 = {'supermarkt': "Albert Heijn", 'wat': "haring", 'korting': "25 procent" , 'hoeveel': "2 a 3 stuks", 'normaal': "2 euro en 50 cent", 'deze week': "1 euro en 87 cent"}
+Visdict3 = {'supermarkt': "Albert Heijn", 'wat': "Gebakken Kibbeling", 'korting': "25 procent" , 'hoeveel': "150 gram", 'normaal': "3 euro en 25 cent ", 'deze week': "2 euro en 43 cent"}
+Visdict4 = {'supermarkt': "Plus", 'wat': "Princes tonijnstukken", 'korting': "45 cent" , 'hoeveel': "160 gram", 'normaal': "1 euro en 45 cent", 'deze week': "1 euro"}
+Visdict5 = {'supermarkt': "Plus", 'wat': "Gerookte noorse zalm", 'korting': "2 euro en 1 cent" , 'hoeveel': "pakje 100 gram", 'normaal': "6 euro", 'deze week': "3 euro en 99 cent"}
+Visdict6 = {'supermarkt': "Plus", 'wat': "Gijs gerookte makreel filet", 'korting': "50 cent" , 'hoeveel': "pakje 175 gram", 'normaal': "3 euro en 99 cent", 'deze week': "3 euro en 49 cent"}
---
+++ Esmee/conceptmap/formatVis.py
@@ -0,0 +1,66 @@
+import Vis
+
+d = Vis.Visdict
+
+print "Supermark: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Vis.Visdict1
+
+print "Supermark: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Vis.Visdict2
+
+print "Supermark: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Vis.Visdict3
+
+print "Supermark: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Vis.Visdict4
+
+print "Supermark: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Vis.Visdict5
+
+print "Supermark: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Vis.Visdict6
+
+print "Supermark: %s." %(d['supermarkt'])
+print "artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+print "Vis is 6x in de aanbieding deze week. "
