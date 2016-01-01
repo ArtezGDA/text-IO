eebfbba5068a30bb59a19c969251f0d3b1d8e546
Esmee Ellson <esmee@ellson.nl>
2016-01-02 00:17:37 +0100

    s

--- Esmee/conceptmap/formatSoep.py
+++ Esmee/conceptmap/formatSoep.py
@@ -3,7 +3,7 @@ import Soep
 d = Soep.Soepdict
 
 print "Supermark: %s." %(d['supermarkt'])
-print "artikel: %s, %s." %(d['wat'])
+print "artikel: %s." %(d['wat'])
 print "Beschrijving: %s." %(d['hoeveel'])
 print "Korting: %s." % (d['korting'])
 print "Van: %s." % (d['normaal'])
