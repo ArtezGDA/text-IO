cebabdeb4f3e97dd11be3384b7b84902ce2966e2
Esmee Ellson <esmee@ellson.nl>
2016-01-02 00:59:23 +0100

    vis

--- Esmee/conceptmap/Dessert.py
+++ Esmee/conceptmap/Dessert.py
@@ -1,10 +1,8 @@
-Dessertdict = 
-
-{'supermarkt': "Spar", 'wat': "Den elder roomyoghurt", 'korting': "1+1 gratis" , 'hoeveel': "400 gram", 'normaal': "2 euro en 98 cent", 'deze week': "1 euro en 49 cent"}
-{'supermarkt': "Jumbo", 'wat': "Mona pudding", 'korting': "58 cent" , 'hoeveel': "2 bakken a 450 ml", 'normaal': "2 euro en 58 cent", 'deze week': "2 euro"}
-{'supermarkt': "Jan Linders", 'wat': "Mona hollandse verwenyoghurt", 'korting': "50 cent" , 'hoeveel': "beker 500 gram", 'normaal': "1 euro en 59 cent", 'deze week': "99 cent"}
-{'supermarkt': "Jan Linders", 'wat': "Danone danio", 'korting': "20 cent" , 'hoeveel': "250 gram", 'normaal': "1 euro en 19 cent", 'deze week': "99 cent"}
-{'supermarkt': "Jan Linders", 'wat': "Zuivelhoeve Boer'n yoghurt", 'korting': "1+1 gratis" , 'hoeveel': "450 gram", 'normaal': "2 euro en 90 cent", 'deze week': "1 euro en 45 cent"}
-{'supermarkt': "Albert Heijn", 'wat': "Sorbetijs", 'korting': "25 procent" , 'hoeveel': "500 ml", 'normaal': "1 euro en 85 cent", 'deze week': "1 euro en 39 cent"}
-{'supermarkt': "Albert Heijn", 'wat': "Campina", 'korting': "5 cent" , 'hoeveel': "per pak", 'normaal': "1 euro en 5 cent", 'deze week': "1 euro"}
-{'supermarkt': "Plus", 'wat': "Optimel yoghurt", 'korting': "57 cent" , 'hoeveel': "2 stuks", 'normaal': "3 euro en 6 cent", 'deze week': "2 euro en 49 cent"}
+Dessertdict = {'supermarkt': "Spar", 'wat': "Den elder roomyoghurt", 'korting': "1+1 gratis" , 'hoeveel': "400 gram", 'normaal': "2 euro en 98 cent", 'deze week': "1 euro en 49 cent"}
+Dessertdict1 = {'supermarkt': "Jumbo", 'wat': "Mona pudding", 'korting': "58 cent" , 'hoeveel': "2 bakken a 450 ml", 'normaal': "2 euro en 58 cent", 'deze week': "2 euro"}
+Dessertdict2 = {'supermarkt': "Jan Linders", 'wat': "Mona hollandse verwenyoghurt", 'korting': "50 cent" , 'hoeveel': "beker 500 gram", 'normaal': "1 euro en 59 cent", 'deze week': "99 cent"}
+Dessertdict3 = {'supermarkt': "Jan Linders", 'wat': "Danone danio", 'korting': "20 cent" , 'hoeveel': "250 gram", 'normaal': "1 euro en 19 cent", 'deze week': "99 cent"}
+Dessertdict4 = {'supermarkt': "Jan Linders", 'wat': "Zuivelhoeve Boer'n yoghurt", 'korting': "1+1 gratis" , 'hoeveel': "450 gram", 'normaal': "2 euro en 90 cent", 'deze week': "1 euro en 45 cent"}
+Dessertdict5 = {'supermarkt': "Albert Heijn", 'wat': "Sorbetijs", 'korting': "25 procent" , 'hoeveel': "500 ml", 'normaal': "1 euro en 85 cent", 'deze week': "1 euro en 39 cent"}
+Dessertdict6 = {'supermarkt': "Albert Heijn", 'wat': "Campina", 'korting': "5 cent" , 'hoeveel': "per pak", 'normaal': "1 euro en 5 cent", 'deze week': "1 euro"}
+Dessertdict7 = {'supermarkt': "Plus", 'wat': "Optimel yoghurt", 'korting': "57 cent" , 'hoeveel': "2 stuks", 'normaal': "3 euro en 6 cent", 'deze week': "2 euro en 49 cent"}
