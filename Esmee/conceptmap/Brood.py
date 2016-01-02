412a9d1074e4823a3543f094927bb16a97688040
Esmee Ellson <esmee@ellson.nl>
2016-01-02 01:16:00 +0100

    brood

--- Esmee/conceptmap/Brood.py
+++ Esmee/conceptmap/Brood.py
@@ -1,16 +1,14 @@
-Brooddict = 
-
-{'supermarkt': "Spar", 'wat': "italiaanse bollen", 'korting': "66 cent", 'hoeveel': "3 stuks", 'normaal': "1 euro en 65 cent", 'deze week': "99 cent"}
-{'supermarkt': "Spar", 'wat': "Ambachtelijk fijn of grof volkoren", 'korting': "61 cent" , 'hoeveel': "heel", 'normaal': "1 euro en 80 cent", 'deze week': "1 euro en 19 cent"}
-{'supermarkt': "Jan Linders", 'wat': "Korengoud luxe stokbroden", 'korting': "20 cent" , 'hoeveel': "1 stuk", 'normaal': "1 euro en 29 cent", 'deze week': "99 cent"}
-{'supermarkt': "Jan Linders", 'wat': "Korengoud bus brood", 'korting': "20 cent" , 'hoeveel': "1 stuk", 'normaal': "1 euro en 49 cent", 'deze week': "1 euro en 29 cent"}
-{'supermarkt': "Jan Linders", 'wat': "Korengoud melkbollen", 'korting': "20 cent" , 'hoeveel': "zak 6 stuks", 'normaal': "1 euro en 29 cent", 'deze week': "99 cent"}
-{'supermarkt': "Jan Linders", 'wat': "Worstenbroodjes", 'korting': "1 euro en 99 cent" , 'hoeveel': "zak 4 stuks", 'normaal': "3 euro en 98 cent", 'deze week': "1 euro en 99 cent"}
-{'supermarkt': "Albert Heijn", 'wat': "Hartige mini hapjes", 'korting': " 26 cent" , 'hoeveel': "240 gram", 'normaal': "1 euro en 65 cent", 'deze week': "1 euro en 39 cent"}
-{'supermarkt': "Albert Heijn", 'wat': "Steekbroden", 'korting': "26 cent", 'hoeveel': "heel", 'normaal': "1 euro en 75 cent", 'deze week': "1 euro en 49 cent"}
-{'supermarkt': "Plus", 'wat': "Ciabatta", 'korting': "15 cent", 'hoeveel': "250 gram", 'normaal': "1 euro en 15 cent", 'deze week': "1 euro"}
-{'supermarkt': "Plus", 'wat': "Kaiserbroodjes", 'korting': "20 cent", 'hoeveel': "4 stuks", 'normaal': "1 euro en 20 cent", 'deze week': "1 euro"}
-{'supermarkt': "Plus", 'wat': "Meggie baguette", 'korting': "29 cent", 'hoeveel': "160 gram", 'normaal': "1 euro en 29 cent", 'deze week': "1 euro"}
-{'supermarkt': "Plus", 'wat': "Korenlanders rond", 'korting': "40 cent" , 'hoeveel': "heel", 'normaal': "1 euro en 89 cent", 'deze week': "1 euro en 49 cent"}
-{'supermarkt': "Plus", 'wat': "Bollentjes", 'korting': "11 cent" , 'hoeveel': "zak 6 stuks", 'normaal': "1 euro", 'deze week': "89 cent"}
-{'supermarkt': "Lidl", 'wat': "Party broodjes", 'korting': "30 cent" , 'hoeveel': "270 gram", 'normaal': "1 euro en 99 cent", 'deze week': "69 cent"}
+Brooddict = {'supermarkt': "Spar", 'wat': "italiaanse bollen", 'korting': "66 cent", 'hoeveel': "3 stuks", 'normaal': "1 euro en 65 cent", 'deze week': "99 cent"}
+Brooddict1 = {'supermarkt': "Spar", 'wat': "Ambachtelijk fijn of grof volkoren", 'korting': "61 cent" , 'hoeveel': "heel", 'normaal': "1 euro en 80 cent", 'deze week': "1 euro en 19 cent"}
+Brooddict2 = {'supermarkt': "Jan Linders", 'wat': "Korengoud luxe stokbroden", 'korting': "20 cent" , 'hoeveel': "1 stuk", 'normaal': "1 euro en 29 cent", 'deze week': "99 cent"}
+Brooddict3 = {'supermarkt': "Jan Linders", 'wat': "Korengoud bus brood", 'korting': "20 cent" , 'hoeveel': "1 stuk", 'normaal': "1 euro en 49 cent", 'deze week': "1 euro en 29 cent"}
+Brooddict4 = {'supermarkt': "Jan Linders", 'wat': "Korengoud melkbollen", 'korting': "20 cent" , 'hoeveel': "zak 6 stuks", 'normaal': "1 euro en 29 cent", 'deze week': "99 cent"}
+Brooddict5 = {'supermarkt': "Jan Linders", 'wat': "Worstenbroodjes", 'korting': "1 euro en 99 cent" , 'hoeveel': "zak 4 stuks", 'normaal': "3 euro en 98 cent", 'deze week': "1 euro en 99 cent"}
+Brooddict6 = {'supermarkt': "Albert Heijn", 'wat': "Hartige mini hapjes", 'korting': " 26 cent" , 'hoeveel': "240 gram", 'normaal': "1 euro en 65 cent", 'deze week': "1 euro en 39 cent"}
+Brooddict7 = {'supermarkt': "Albert Heijn", 'wat': "Steekbroden", 'korting': "26 cent", 'hoeveel': "heel", 'normaal': "1 euro en 75 cent", 'deze week': "1 euro en 49 cent"}
+Brooddict8 = {'supermarkt': "Plus", 'wat': "Ciabatta", 'korting': "15 cent", 'hoeveel': "250 gram", 'normaal': "1 euro en 15 cent", 'deze week': "1 euro"}
+Brooddict9 = {'supermarkt': "Plus", 'wat': "Kaiserbroodjes", 'korting': "20 cent", 'hoeveel': "4 stuks", 'normaal': "1 euro en 20 cent", 'deze week': "1 euro"}
+Brooddict10 = {'supermarkt': "Plus", 'wat': "Meggie baguette", 'korting': "29 cent", 'hoeveel': "160 gram", 'normaal': "1 euro en 29 cent", 'deze week': "1 euro"}
+Brooddict11 = {'supermarkt': "Plus", 'wat': "Korenlanders rond", 'korting': "40 cent" , 'hoeveel': "heel", 'normaal': "1 euro en 89 cent", 'deze week': "1 euro en 49 cent"}
+Brooddict12 = {'supermarkt': "Plus", 'wat': "Bollentjes", 'korting': "11 cent" , 'hoeveel': "zak 6 stuks", 'normaal': "1 euro", 'deze week': "89 cent"}
+Brooddict13 = {'supermarkt': "Lidl", 'wat': "Party broodjes", 'korting': "30 cent" , 'hoeveel': "270 gram", 'normaal': "1 euro en 99 cent", 'deze week': "69 cent"}
---
+++ Esmee/conceptmap/formatChips.py
@@ -0,0 +1,48 @@
+import Chips
+
+d = Chips.Chipsdict
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Chips.Chipsdict1
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Chips.Chipsdict2
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Chips.Chipsdict3
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Chips.Chipsdict4
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+print "Chips is 5x in de aanbieding deze week."
