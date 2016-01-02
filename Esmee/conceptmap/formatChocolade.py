c30389e07c37d0cd242598bd95f3a845f3831e10
Esmee Ellson <esmee@ellson.nl>
2016-01-02 01:08:15 +0100

    choc

---
+++ Esmee/conceptmap/formatChocolade.py
@@ -0,0 +1,30 @@
+import Chocolade
+
+d = Chocolade.Chocoladedict
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Chocolade.Dessertdict1
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+d = Chocolade.Dessertdict2
+
+print "Supermarkt: %s." %(d['supermarkt'])
+print "Artikel: %s." %(d['wat'])
+print "Beschrijving: %s." %(d['hoeveel'])
+print "Korting: %s." % (d['korting'])
+print "Van: %s." % (d['normaal'])
+print "Voor: %s." % (d['deze week'])
+
+print "Chocolade is 3x in de aanbieding deze week."
