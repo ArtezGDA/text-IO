bb61fde3602cf35046e58c126bd05e4a6a021a16
Leonie Gores <leonie-gores@hotmail.com>
2016-01-06 22:04:33 +0100

    ...

--- Leonie/freemoney/money2.py
+++ Leonie/freemoney/money2.py
@@ -1,5 +1,6 @@
 import dollar
 
+#defining the functions
 txt = "twerk"
 output = [ "", "", "", "", ""]
 
@@ -7,10 +8,13 @@ char = p
 
 font [char] [0]
 
+#making a loop to make sure all the letters will be placed in a scentence next to eachother
 for char in txt:
 	for i in range(5):
 		output[i] = output[i] +
 		 	font [char][i]
 
+
+#Printing the letters
 for i in range(5)
 	print output[i]
