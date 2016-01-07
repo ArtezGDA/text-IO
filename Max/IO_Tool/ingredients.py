a6270ee7c299dad2eaaa62bac7f09bb042dd35fe
Max Waelbers <maxwaelbers@MacBook-Pro-van-Max.local>
2016-01-07 14:01:15 +0100

    edit

--- Max/IO_Tool/ingredients.py
+++ Max/IO_Tool/ingredients.py
@@ -4,4 +4,14 @@ ingredients = {
 		'hoofdgerecht': "AH rundergehakt 300 gAH mager rundergehakt 300 g",
 		'nagerecht': "Mona Gezingspudding griesmeel met saus 500 g", 
 		'drinken': "AH Zuid-afrika huiswijn fris", 
+
+		'voorgerecht': "AH Greenfields rosbief naturel",
+		'hoofdgerecht': "AH Biologisch Kipfiletblokjes cd 250g",
+		'nagerecht': "Mona Gezingspudding griesmeel met saus 500 g", 
+		'drinken': "Arizona Green tea honey", 
+
+		'voorgerecht': "AH Biologisch Jonge kaas 50+ plakken",
+		'hoofdgerecht': "Zeelands Roem Kreeft",
+		'nagerecht': "Mona Gezingspudding griesmeel met saus 500 g", 
+		'drinken': "AH Vers sap sinaasappel", 
 	}
--- Max/IO_Tool/recipe.py
+++ Max/IO_Tool/recipe.py
@@ -1,10 +1,35 @@
+#import json
+#import random
+
 import ingredients #import bestanden uit ingredients.py
 
 #whatarewegoingtoeat.py
-
+#def randomhoofdgerecht():
+#	jsonfile = "hoofdgerecht.json"
+###
+	#	return random.choice(data)
 d = ingredients. ingredients #linkt an string uit my.data bestand
 
+#def main():
+#	random_hoofd = randomhoofdgerecht()
+	#while True:
+print "menu 1"
 print "voorgerecht: %s" % (d ["voorgerecht"])
 print "hoofdgerecht: %s" % (d ["hoofdgerecht"])
 print "nagerecht: %s" % (d ["nagerecht"])
 print "drinken: %s" % (d ["drinken"])
+
+print "menu 2"
+print "voorgerecht: %s" % (d ["voorgerecht"])
+print "hoofdgerecht: %s" % (d ["hoofdgerecht"])
+print "nagerecht: %s" % (d ["nagerecht"])
+print "drinken: %s" % (d ["drinken"])
+
+print "menu 3"
+print "voorgerecht: %s" % (d ["voorgerecht"])
+print "hoofdgerecht: %s" % (d ["hoofdgerecht"])
+print "nagerecht: %s" % (d ["nagerecht"])
+print "drinken: %s" % (d ["drinken"])
+
+	#if __name__== '__main__':
+		#3main()
