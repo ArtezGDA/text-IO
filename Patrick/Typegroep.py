b5d3478bef4dbc70fdf1fa9a76216a8016bacafa
irlabs <developer@irlabs.nl>
2016-01-28 14:49:46 +0100

    Fixed the Type structure for Patrick's group and everybody else

---
+++ Patrick/Typegroep.py
@@ -0,0 +1,100 @@
+typeface = {
+    'font': 'Gotham',
+    "foundry": "Hoefler",
+    "designer": "tobias frere-jones",
+    "cassification": "geometric sans-serif",
+    "default-size": 12.0, 
+    'weights': [
+        {'name': "bold", 'weight': 600},
+        {'name': "medium", 'weight': 500},
+        {'name': "light", 'weight': 350}
+    ],
+    'characters': [
+        {
+            'character': "A",
+            'weight': "bold",
+            'font-set' : "uppercase",
+            "compoundshapes":[
+                [
+                    {'x': 16.767, 'y': -19.182},
+                    {'x': 33.395, 'y': -19.182,
+                        'd1': {'x': 11, 'y': 22},
+                        'd2': {'x': 11, 'y': 22}},
+                    {'x': 25.047, 'y': -38.501}
+                ]
+            ],
+            "outlines":[
+                [
+                    {'x': 0, 'y': 0},
+                    {'x': 0, 'y': 8.694},
+                    {'x': 13.662, 'y': -11.661},
+                    {'x': 36.52, 'y': -11.6611},
+                    {'x': 41.399, 'y': 0},
+                    {'x': 50.369, 'y': 0},
+                    {'x': 29.117, 'y': -48.644},
+                    {'x': 21.252, 'y': -48.644}
+                ],
+            ],
+        },
+        {
+            'character': "B",
+            'weight': "bold",
+            'font-set' : "uppercase",
+            "compoundshapes":[
+                [
+                    {'x': 16.767, 'y': -19.182},
+                    {'x': 33.395, 'y': -19.182},
+                    {'x': 25.047, 'y': -38.501}
+                ],
+                [
+                    {'x': 16.767, 'y': -19.182},
+                    {'x': 33.395, 'y': -19.182},
+                    {'x': 25.047, 'y': -38.501}
+                ]
+            ],
+            "outlines":[
+                [
+                    {'x': 0, 'y': 0},
+                    {'x': 0, 'y': 8.694,
+                        'd1': {'x': 3.0, 'y': 0.575},
+                        'd2': {'x': 5.0, 'y': 67.046}},
+                    {'x': 13.662, 'y': -11.661},
+                    {'x': 36.52, 'y': -11.6611},
+                    {'x': 41.399, 'y': 0},
+                    {'x': 50.369, 'y': 0},
+                    {'x': 29.117, 'y': -48.644},
+                    {'x': 21.252, 'y': -48.644}
+                ],
+            ],
+        },
+        {
+            'character': "a",
+            'weight': "bold",
+            'font-set' : "lowercase",
+            "compoundshapes":[
+                ["..."]
+            ],
+            "outlines":[
+                ["..."]
+            ],
+        }
+    ],
+    'kerningpairs': [
+        {'pair': "AB", 'kerning': -10},
+        {'pair': "AC", 'kerning': -10},
+        {'pair': "AD", 'kerning': -10},
+        {'pair': "Ab", 'kerning': -10},
+        {'pair': "Ac", 'kerning': -10},
+        {'pair': "Ad", 'kerning': -10},
+        {'pair': "BA", 'kerning': -10},
+        {'pair': "BB", 'kerning': -10},
+        {'pair': "BC", 'kerning': -10},
+        {'pair': "Ba", 'kerning': -10},
+        {'pair': "Bb", 'kerning': -10},
+        {'pair': "Bc", 'kerning': -10}
+    ]
+}
+                            
+                            
+print typeface
+
